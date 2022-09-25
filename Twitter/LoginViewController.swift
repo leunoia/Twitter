0b09586c2039d1ff2481685cbd5f94711481b5f4
leunoia <prestonbarney123@gmail.com>
2022-09-25 18:52:18 -0400

    first commit

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Preston Barney on 9/25/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error)in
+            print("could not login")
+        })
+    }
+    
+}
---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Preston Barney on 9/25/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
